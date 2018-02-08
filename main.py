23ae5e3e4f4420fbd45f73eb51317456698d4aca
GinYoshida <wood1969@hotmail.com>
2018-02-09 00:53:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,8 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-import imageio
-imageio.plugins.ffmpeg.download()
 from moviepy.editor import VideoFileClip
 import functionset as myfc
-
 
 def main():
     '''Main function to execute following 2 procedure
@@ -26,21 +23,18 @@ def main():
     # images = ['straight_lines1','straight_lines2','test1','test2','test3','test4','test5','test6','test7',
     #           'test8','test9','test10','test11']
     #
-#    images = ['test10','test9','test8','test11']
+    # images = ['test10','test9','test8','test11']
 # # , 'test9', 'test10']
 
-#    for file_path in images:
-#       myfc.image_converter(file_path)
+    # for file_path in images:
+    #    myfc.image_converter(file_path)
 
     # Part for step2
-    clip1 = VideoFileClip("project_video.mp4").subclip(38.43)
-    # clip1 = VideoFileClip("test_videos/solidWhiteRight.mp4")
-    white_clip = clip1.fl_image(myfc.pipeline_video)
-    white_clip.write_videofile("project_video_w_pipeline.mp4", audio=False)
+    myfc.video_creation("project_video.mp4","project_video_w_pipeline.mp4",42,40,False)
 
 if __name__ == "__main__":
     # execute only if run as a script
     import os
-#    os.chdir("C:/Users/hitoshi/AppData/Local/Programs/Python/" +
-#             "Python35/Scripts/Udacity/CarND_t1_P4_Adv_lane_finding")
+    os.chdir("C:/Users/hitoshi/AppData/Local/Programs/Python/" +
+             "Python35/Scripts/Udacity/CarND_t1_P4_Adv_lane_finding")
     main()
